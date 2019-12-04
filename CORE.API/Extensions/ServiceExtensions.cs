900acce3ed7c949f98b2a97cfdd9befb6afadd0a
faisal.ayub@ebitlogix.com <faisal.ayub@ebitlogix.com>
2019-12-04 16:24:14 +0500

    changes

---
+++ CORE.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Core.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CORE.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+
+        public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<RepositoryContext>(options =>
+             options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+        }
+
+    }
+}
